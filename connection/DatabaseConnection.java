16b491a6f5e9ecca591f4444d046f72265d374d7
Sebastian Burgos Puentes <sebastian.burgosp@uqvirtual.edu.co>
2023-08-18 12:04:40 -0500

    finalized project

---
+++ connection/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package multinivel.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private final String stringConexion = "jdbc:oracle:thin:@localhost:1521:xe";
+    private String driver = "oracle.jdbc.driver.OracleDriver";
+    private Connection cx;
+    private String usr = "MULTINIVEL_DBA";
+    private String passwd = "1234";
+
+    public Connection conectar(){
+        try{
+            Class.forName(driver);
+            cx = DriverManager.getConnection(stringConexion, usr, passwd);
+            System.out.println("SE HA LEVANTADO LA CONEXION A LA BASE DE DATOS CORRECTAMENTE");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("NO SE HA LEVANTADO LA CONEXION A LA BASE DE DATOS");
+        }
+        return cx;
+    }
+}
